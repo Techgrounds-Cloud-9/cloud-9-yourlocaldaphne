6bbfd3f3427403182ed4fc5a5eca91566697ebb2
Daphne <daphnedemoet@gmail.com>
2022-10-19 20:34:48 +0200

    finished exercise 02 of PRG08

--- 02_Python_1/sprint03_week_04/PRG08_key_value_pairs/exercise_02.py
+++ 02_Python_1/sprint03_week_04/PRG08_key_value_pairs/exercise_02.py
@@ -1,20 +1,18 @@
 import csv
-from re import A
-
 
 info = ["First name", "Last name", "Job title", "Company"]
 
-first_name = (input("Enter your first name:"))
+first_name = input("Enter your first name:")
 last_name = input("Enter your last name:")
 job_title = input("Enter your job title:")
 company = input("Enter your company:")
 
-Dict = [{
+Dict = {
     "First name": first_name,
     "Last name": last_name,
     "Job title": job_title,
     "Company": company
-    }]
+    }
 
 print(Dict)
 
@@ -23,6 +21,6 @@ csv_file = open('exercise_02.csv', 'a')
 writer = csv.writer(csv_file)
 writer.writerow(Dict.values())
 csv_file.close()
-csv_file = open('exercise_02.csv', 'a')
+csv_file = open('exercise_02.csv', 'r')
 print(csv_file.read())
 
